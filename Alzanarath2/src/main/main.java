1f2b5ecc8b44ee51d5eb898003536bbf3f68719a
Cristian <108484399+PL4Y3Rx@users.noreply.github.com>
2024-09-03 17:11:40 +0300

    Networking Basic Added
    It's yet in development, some errors appears, but I'll fix

--- Alzanarath2/src/main/main.java
+++ Alzanarath2/src/main/main.java
@@ -1,13 +1,19 @@
 package main;
 
 import javax.swing.JFrame;
+import Networking.Configuration;
+import Networking.NetworkManager;
 
 public class main {
 	private static JFrame window;
 
 	public static void main(String[] args) {
+		Configuration config = new Configuration(5050, "127.0.0.1");
+		boolean isServer = false;
+		NetworkManager networkManager = new NetworkManager(isServer, config);
+		
 		window = new JFrame();
-		GamePanel gp = new GamePanel();
+		GamePanel gp = new GamePanel(networkManager);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setTitle("Alzanarath 2");
