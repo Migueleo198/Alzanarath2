29cdbf607914994fbc5c71da24ea77d3f55322ed
Migueleo198 <miguelruiznavarro11@gmail.com>
2024-09-10 04:35:44 +0200

    update

--- Alzanarath2/src/Networking/NetworkManager.java
+++ Alzanarath2/src/Networking/NetworkManager.java
@@ -456,10 +456,9 @@ public class NetworkManager {
     public String promptInputName(String role) {
         String name;
         while (true) {
-            name = JOptionPane.showInputDialog(this,"Introduce your name");
+            name = JOptionPane.showInputDialog(null, "Input your username:", "Username Input", JOptionPane.PLAIN_MESSAGE);
             if (name == null || name.trim().isEmpty() || name.length() > 8) {
-            	JOptionPane.showMessageDialog(gamePanel, "Introduce a valid name");
-            	name = JOptionPane.showInputDialog(this,"Introduce your name");
+                JOptionPane.showMessageDialog(null, "Invalid username. Please enter a valid name.", "Error", JOptionPane.ERROR_MESSAGE);
             } else {
                 return name;
             }
